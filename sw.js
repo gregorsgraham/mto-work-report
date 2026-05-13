3968aa391bc3b591f23b105c2a0230e40dccf441
Gregor Graham <gregorsgraham@gmail.com>
2026-05-12 23:36:12 -0700

    Deploy 2026-05-12 23:36

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 // old caches are deleted. The SW listens for SKIP_WAITING messages so the
 // page can promote a new SW immediately after install.
 
-const CACHE_VERSION = 'mto-v74';
+const CACHE_VERSION = 'mto-v77';
 const SHELL = [
   './',
   './index.html',
